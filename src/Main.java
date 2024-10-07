3ec1650ca53305664f212d5ba4e2b61f809d7c20
s30457 <s30457@pjwstk.edu.pl>
2024-10-07 11:01:02 +0200

    Initial project setup

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // TODO: we need to add the missing classes!
 
-// OK, I will add `Adder` and s30457 will add `Subtractor`.
+// OK, I will add `Adder` and s31766 will add `Subtractor`.
 
 public class Main {
     public static void main(String[] args) {
